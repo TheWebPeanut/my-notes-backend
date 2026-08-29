bc8cb615f0ad9f362402077b2649e06d4dd4f061
TheWebPeanut <emilio.elenes@icloud.com>
2026-08-29 22:37:34 +0200

    build: add mongodb connection string

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot:4.1.1")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
